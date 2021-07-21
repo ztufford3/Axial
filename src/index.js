ad7d9b1c10c7fc09c027c630227df9c9ba153216
Zack Tufford <ztufford3@gmail.com>
2021-07-21 19:11:15 -0400

    initialize typescript and react-router, set up initial route and constants

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+import { routes } from './utils/constants';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path={`/${routes.FINANCIAL_NUMBER_INPUT}`}>
+          <input />
+        </Route>
+        <Route path="/" render={() => <Redirect to={`/${routes.FINANCIAL_NUMBER_INPUT}`} />} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/utils/constants.ts
@@ -0,0 +1,12 @@
+const financialAbbreviationMap = {
+    'k': 1000,
+    'm': 1000000,
+    'b': 1000000000,
+};
+
+const routes = {
+    FINANCIAL_NUMBER_DISPLAY: 'FinancialNumberDisplay',
+    FINANCIAL_NUMBER_INPUT: 'FinancialNumberInput',
+};
+
+export { financialAbbreviationMap, routes };
